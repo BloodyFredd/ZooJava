464adb664e55a9d9e244a43e01791fd18a7f7314
Freddy <fredtein@gmail.com>
2017-06-17 23:13:53 +0300

    picture path

--- graphics/IDrawable.java
+++ graphics/IDrawable.java
@@ -8,7 +8,7 @@ import java.awt.Graphics;
  * campus beer-sheva
  */
 public interface IDrawable {
-	 public final static String PICTURE_PATH = "C:/Users/Freddy/Desktop/Eclipse/Workspace/Zoo2/photos";
+	 public final static String PICTURE_PATH = "Type_Here";
 	 public void loadImages(String nm);
 	 public void drawObject (Graphics g);
 	 public String getColor();	 
